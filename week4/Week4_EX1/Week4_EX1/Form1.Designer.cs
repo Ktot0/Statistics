95c1075a1446d55061ce24ffb486ef43f9c0db3c
Ktot0 <57805239+Ktot0@users.noreply.github.com>
2022-10-22 16:22:33 +0200

    Add files via upload

---
+++ week4/Week4_EX1/Week4_EX1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Week4_EX1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Relative Frequency";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(800, 450);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Absolute Frequency";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Normalized Frequency";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 28);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Coin Tosser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ week4/Week4_EX1/Week4_EX1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Markup;
+
+namespace Week4_EX1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> units = new List<int>();
+            List<double> freq = new List<double>();
+            Random random = new Random();
+            for(int i = 0; i < 101; i++)
+            {
+                units.Add(random.Next(0, 2));
+            }
+
+
+
+            int succ = 0;
+            int total = 1;
+            for(int i = 0; i < 101; i++)
+            {
+                if (units[i] == 0)
+                {
+                    succ++;
+                }        
+                double res = (double)succ / total;
+                freq.Add(res);
+                total++;
+
+            }
+
+            var series1 = new LiveCharts.Wpf.LineSeries()
+            {
+                Title = "Relative Frequency",
+                Values = new LiveCharts.ChartValues<double>(freq),
+            };
+
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> units = new List<int>();
+            List<int> absolute = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 101; i++)
+            {
+                units.Add(random.Next(0, 2));
+            }
+
+
+
+            int succ = 0;
+            for (int i = 0; i < 101; i++)
+            {
+                if (units[i] == 1)
+                {
+                    succ++;
+                }
+
+                absolute.Add(succ);
+            }
+
+            var series1 = new LiveCharts.Wpf.LineSeries()
+            {
+                Title = "Absolute Frequency",
+                Values = new LiveCharts.ChartValues<int>(absolute),
+            };
+
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<int> units = new List<int>();
+            List<double> freq = new List<double>();
+            Random random = new Random();
+            for (int i = 0; i < 101; i++)
+            {
+                units.Add(random.Next(0, 2));
+            }
+
+
+
+            int succ = 0;
+            int total = 1;
+            for (int i = 0; i < 101; i++)
+            {
+                if (units[i] == 0)
+                {
+                    succ++;
+                }
+                double res = (double)succ / Math.Sqrt(total);
+                freq.Add(res);
+                total++;
+
+            }
+
+            var series1 = new LiveCharts.Wpf.LineSeries()
+            {
+                Title = "Normalized Frequency",
+                Values = new LiveCharts.ChartValues<double>(freq),
+            };
+
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(series1);
+        }
+    }
+}
